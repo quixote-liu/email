06cabcf0cb07ac90d36898e430b045866f606125
lcs <lcs.shun@foxmail.com>
2022-06-11 18:18:39 +0800

    opt

--- example/main.go
+++ example/main.go
@@ -58,6 +58,12 @@ func SendHTML() {
 	if err := e.AttachFile("./shenghuo.txt"); err != nil {
 		log.Fatal(err)
 	}
+	if err := e.AttachFile("./photo.png"); err != nil {
+		log.Fatal(err)
+	}
+	if err := e.AttachFile("./email"); err != nil {
+		log.Fatal(err)
+	}
 
 	err := e.Send()
 	if err != nil {
